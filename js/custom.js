cc4dce4d09153e16f92079aa6c789f4864fd378e
Pudge1996 <448404152@qq.com>
2018-01-04 17:47:43 +0800

    test

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(window).load(function () {
 
     // preloader
     $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(550).css({
         'overflow': 'visible'
     });
